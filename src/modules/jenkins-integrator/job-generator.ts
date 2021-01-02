965965936566815f06cd5148f89fd7c7069ac5c3
Iresh M Dissanayaka <socialexpz1@gmail.com>
2021-01-02 19:59:16 +0530

    generateJobConfig implementation

--- src/modules/jenkins-integrator/job-generator.ts
+++ src/modules/jenkins-integrator/job-generator.ts
@@ -1,11 +1,10 @@
-function generateJob(
+export function generateJobConfig(
   templateJob: string,
   artifactIdentifier: string,
   callBackUrl: string,
-  projectName: string,
-  repoUrl?: string
+  nexusRepoUrl: string
 ): string {
-  let result = templateJob;
-
+  let result = templateJob.replace('ARTIFACTIDENTIFIER', artifactIdentifier)
+    .replace('CALLBACKURL', callBackUrl).replace('NEXUSREPOURL', nexusRepoUrl)
   return result;
 }
